f8bc10978fd01716c0c8110119a6905b119d2ebf
alphacode007 <64978977+alphacode007@users.noreply.github.com>
2024-01-09 16:45:38 +0530

    new

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  method: 'GET',
+  url: 'https://youtube-v31.p.rapidapi.com/captions',
+  params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
+  headers: {
+    'X-RapidAPI-Key': 'a1ed9a8ac3msh654b030d54cf8fbp1c48dbjsnff4ee088bf89',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
